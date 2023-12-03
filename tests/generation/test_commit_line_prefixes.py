65e1331a82fd6938a53901ed3d170c571f3416ce
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-12-02 19:31:51 -0500

    Update Documentation (#2)
    Documentation:
    * Update README.md
    
    Tests:
    * Update test_commit_line_prefixes.py - replace usages of assertEquals with assertEqual
    * Update test_commit_line.py - improve test case method names, rename assertEquals usages to assertEqual
    
    Workflow:
    * Update ci_run.yml - add python version 3.12

--- tests/generation/test_commit_line_prefixes.py
+++ tests/generation/test_commit_line_prefixes.py
@@ -4,55 +4,60 @@ from text.generation.commit_line_prefixes import update_line
 
 
 class TestCommitLinePrefixes(unittest.TestCase):
-    """Test that commit messages have the correct line prefixes."""
+    """Test that commit messages have the correct line prefixes.
+    """
 
     def test_update_line_no_space(self):
         expected = "* Update README.md - reformat file"
         line = "*u README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line),
         )
         line = "*U README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
 
     def test_update_line_with_space(self):
         expected = "* Update README.md - reformat file"
         line = "* U README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
 
     def test_update_line_extra_prefix_no_space(self):
         expected = "* Update README.md - reformat file"
         line = "*Up README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
         line = "*up README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
 
     def test_update_line_extra_prefix_with_space(self):
         expected = "* Update README.md - reformat file"
         line = "* Up README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
         line = "* up README.md - reformat file"
-        self.assertEquals(
+        self.assertEqual(
             expected, update_line(line)
         )
 
     def test_update_line_invalid_prefix(self):
         line = "* X README.md - reformat file"
-        self.assertIsNone(update_line(line))
+        self.assertIsNone(
+            update_line(line)
+        )
 
     def test_update_line_already_valid_prefix(self):
         line = "* Update README.md - reformat file"
-        self.assertIsNone(update_line(line))
+        self.assertIsNone(
+            update_line(line)
+        )
 
 
 if __name__ == '__main__':
