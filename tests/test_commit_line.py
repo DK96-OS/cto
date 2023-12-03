7c8b266bf2be41b141094ba50d3b28c178dc5743
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-12-02 19:26:24 -0500

    Tests:
    *Update test_commit_line.py - improve test case method names, rename assertEquals usages to assertEqual

--- tests/test_commit_line.py
+++ tests/test_commit_line.py
@@ -3,6 +3,9 @@ from text.commit_line import CommitLine
 
 
 class TestCommitLine(unittest.TestCase):
+    """ Testing the Commit Line Class.
+    The Test setup includes a basic Commit Line input, consisting of a subject and content combined into a simple input.
+    """
 
     def setUp(self) -> None:
         self.subjectA = "A Subject"
@@ -13,92 +16,94 @@ class TestCommitLine(unittest.TestCase):
         self.input = "Subject - Content"
         self.commit_line = CommitLine(self.input)
 
-    def test_get_line(self):
-        self.assertEquals(
+    def test_get_line_input_returns_input(self):
+        self.assertEqual(
             self.input,
             self.commit_line.get_input_line()
         )
 
-    def test_get_subject(self):
-        self.assertEquals(
+    def test_get_subject_returns_subject(self):
+        self.assertEqual(
             self.subject,
             self.commit_line.get_subject()
         )
 
-    def test_get_subject_A_minus(self):
-        self.assertEquals(
+    def test_get_subject_A_minus_returns_subject(self):
+        self.assertEqual(
             self.subjectA,
             CommitLine(
                 self.subjectA + ' - ' + self.contentA
             ).get_subject()
         )
-        # Also with no space around separator
-        self.assertEquals(
+        
+    def test_get_subject_A_minus_no_space_returns_subject(self):
+        self.assertEqual(
             self.subjectA,
             CommitLine(
                 self.subjectA + '-' + self.contentA
             ).get_subject()
         )
 
-    def test_get_subject_A_plus(self):
-        self.assertEquals(
+    def test_get_subject_A_plus_returns_subject(self):
+        self.assertEqual(
             self.subjectA,
             CommitLine(
                 self.subjectA + ' + ' + self.contentA
             ).get_subject()
         )
-        # Also with no space around separator
-        self.assertEquals(
+    
+    def test_get_subject_A_plus_returns_subject(self):
+        self.assertEqual(
             self.subjectA,
             CommitLine(
                 self.subjectA + '+' + self.contentA
             ).get_subject()
         )
 
-    def test_get_subject_A_no_separator(self):
-        self.assertEquals(
+    def test_get_subject_A_no_separator_returns_none(self):
+        self.assertEqual(
             None, CommitLine(
                 self.subjectA + ' ' + self.contentA
             ).get_subject()
         )
 
-    def test_get_subject_empty(self):
-        self.assertEquals(
+    def test_get_subject_empty_line_returns_none(self):
+        self.assertEqual(
             None, CommitLine("").get_subject()
         )
 
-    def test_get_content(self):
-        self.assertEquals(
+    def test_get_content_commit_line_returns_content(self):
+        self.assertEqual(
             self.content,
             self.commit_line.get_content()
         )
 
-    def test_get_content_A_minus(self):
-        self.assertEquals(
+    def test_get_content_line_A_minus_returns_content(self):
+        self.assertEqual(
             self.contentA,
             CommitLine(
                 self.subjectA + ' - ' + self.contentA
             ).get_content()
         )
 
-    def test_get_content_A_plus(self):
-        self.assertEquals(
+    def test_get_content_line_A_plus_returns_content(self):
+        self.assertEqual(
             self.contentA,
             CommitLine(
                 self.subjectA + ' + ' + self.contentA
             ).get_content()
         )
 
-    def test_get_content_no_separator(self):
+    def test_get_content_no_separator_returns_line(self):
         line = self.subjectA + ' ' + self.contentA
-        self.assertEquals(
+        self.assertEqual(
             line,
             CommitLine(
                 self.subjectA + ' ' + self.contentA
             ).get_content()
         )
 
-    def test_get_content_empty(self):
+    def test_get_content_empty_line_returns_none(self):
         self.assertIsNone(
             CommitLine("").get_content()
         )
