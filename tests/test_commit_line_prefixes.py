296192a216adc32555da22eb55c62f05f2d964f6
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-03-07 20:31:23 -0500

    Text: *u test_commit_line_prefixes.py - rename method update_line to map_line_prefix

--- tests/test_commit_line_prefixes.py
+++ tests/test_commit_line_prefixes.py
@@ -11,52 +11,52 @@ class TestCommitLinePrefixes(unittest.TestCase):
         expected = "* Update README.md - reformat file"
         line = "*u README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line),
+            expected, map_line_prefix(line),
         )
         line = "*U README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
 
     def test_update_line_with_space(self):
         expected = "* Update README.md - reformat file"
         line = "* U README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
 
     def test_update_line_extra_prefix_no_space(self):
         expected = "* Update README.md - reformat file"
         line = "*Up README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
         line = "*up README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
 
     def test_update_line_extra_prefix_with_space(self):
         expected = "* Update README.md - reformat file"
         line = "* Up README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
         line = "* up README.md - reformat file"
         self.assertEqual(
-            expected, update_line(line)
+            expected, map_line_prefix(line)
         )
 
     def test_update_line_invalid_prefix(self):
         line = "* X README.md - reformat file"
         self.assertIsNone(
-            update_line(line)
+            map_line_prefix(line)
         )
 
     def test_update_line_already_valid_prefix(self):
         line = "* Update README.md - reformat file"
         self.assertIsNone(
-            update_line(line)
+            map_line_prefix(line)
         )
 
 
