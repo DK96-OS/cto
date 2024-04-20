d5cb34eba7831b2f6a0bc1baaef702ab3414b454
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-04-20 12:06:02 -0400

    Commit Text Organizer Package Tests: *c test_argument_parser.py *c test_file_validation.py

---
+++ tests/cto/test_file_validation.py
@@ -0,0 +1,83 @@
+"""Testing File Validation Methods.
+"""
+import pytest
+from pathlib import Path
+
+from commit_text_organizer.file_validation import validate_input_file, validate_name
+
+
+@pytest.mark.parametrize(
+    "test_input,expect",
+    [
+        ("file_name", "file_data"),
+        ("file_name12", "file_data"),
+    ]
+)
+def test_validate_input_file_returns_data(test_input, expect):
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'read_text', lambda _: "file_data")
+        assert validate_input_file(test_input) == expect
+
+
+def test_validate_input_file_does_not_exist_raises_exit():
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: False)
+        try:
+            validate_input_file("file_name")
+            assert False
+        except SystemExit as e:
+            assert True
+
+
+def test_validate_input_file_is_empty_raises_exit():
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'read_text', lambda _: "")
+        try:
+            validate_input_file("file_name")
+            assert False
+        except SystemExit as e:
+            assert True
+
+
+@pytest.mark.parametrize(
+    "test_input,expect",
+    [
+        ("file_name", "file_data"),
+        ("file_name12", "file_data"),
+    ]
+)
+def test_validate_input_file_is_empty_returns_none(test_input, expect):
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'read_text', lambda _: expect)
+        assert validate_input_file(test_input) == expect
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (None),
+        (4),
+        ({}),
+        ([]),
+        (""),
+        (" "),
+        ("\n"),
+    ]
+)
+def test_validate_name_returns_false(test_input):
+    assert validate_name(test_input) == False
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        ("1"),
+        ("a"),
+        ("test"),
+    ]
+)
+def test_validate_name_returns_true(test_input):
+    assert validate_name(test_input) == True
