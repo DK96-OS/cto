ad88f273f4265b8d185f021625f17b48c043d286
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-04-20 21:08:18 -0400

    Commit Text Organizer Package Sample Tests: *u assertions.py - update imports, rename CommitOrganizer

--- tests/cto_samples/assertions.py
+++ tests/cto_samples/assertions.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 from tests.cto_samples import _read_file
-from commit_text_organizer.commit_text_organizer import CommitTextOrganizer
+from commit_text_organizer.commit_organizer import CommitOrganizer
 
 
 def assert_sample_expectations(
@@ -32,7 +32,7 @@ def assert_sample_expectations(
 def load_new_cto(
         file_prefix: str,
         sample_id: int
-) -> CommitTextOrganizer:
+) -> CommitOrganizer:
     """ Load the sample with the given id from the file.
     """
     file_name = str.format(f"{file_prefix}{sample_id}.in")
@@ -41,7 +41,7 @@ def load_new_cto(
         print("Tried to load: " + file_name)
         raise AssertionError(str.format(f"Sample IN #{sample_id} could Not be loaded"))
     #
-    cto = CommitTextOrganizer()
+    cto = CommitOrganizer()
     cto.receive_data(input_data)
     return cto
 
