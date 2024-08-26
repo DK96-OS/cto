73e5a337487c94490ad902c558ac37d06b44a724
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-08-26 16:24:34 -0400

    Project Root: *u setup.py - increment version 1.0.1, specify python minimum version 3.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="commit-text-organizer",
-    version="1.0",
+    version="1.0.1",
     description="A Text Processor Targeted at Organizing Commit Messages.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -24,16 +24,16 @@ setup(
             'cto=commit_text_organizer.__main__:main',
         ],
     },
+    python_requires='>=3.10',
     classifiers=[
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 )
