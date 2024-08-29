b0b9e5a2d129704aa36b5049b3b605a9649853f0
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-08-28 20:29:04 -0400

    Tests: * Create basic_5.expect * Create basic_5.in * Update test_basic.py - add test case

--- tests/cto_samples/test_basic.py
+++ tests/cto_samples/test_basic.py
@@ -4,7 +4,7 @@ from tests.cto_samples.assertions import assert_sample_expectations
 
 # The Sample Prefix points to the input and output files
 _sample_prefix = 'basic/basic_'
-_sample_count = 1
+_sample_count = 5
 
 
 class TestBasic(unittest.TestCase):
@@ -20,6 +20,9 @@ class TestBasic(unittest.TestCase):
 
     def test_sample_4(self):
         assert_sample_expectations(self, _sample_prefix, 4)
+
+    def test_sample_5(self):
+        assert_sample_expectations(self, _sample_prefix, 5)
 
 
 if __name__ == '__main__':
